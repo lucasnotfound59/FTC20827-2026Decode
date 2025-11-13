6e1cc48111ec7540f72577a900f5c41f84fdc49a
lucasnotfound59 <lucasnotfound59@gmail.com>
2025-11-13 21:23:35 +0800

    shootertest

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/ShooterTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/ShooterTest.java
@@ -10,13 +10,19 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 @TeleOp(name="ShooterTest",group="tests")
 @Config
 public class ShooterTest extends LinearOpMode {
-    public static double inputVelocity;
-    private DcMotorEx LeftShooter;
-    private DcMotorEx RightShooter;
+    public static double preShooterVelocity;
+    public static double shooterVelocity;
+    private DcMotorEx preShooter;
+    private DcMotorEx shooter;
     @Override
     public void runOpMode(){
+        preShooter=hardwareMap.get(DcMotorEx.class,"preShooter");
+        shooter=hardwareMap.get(DcMotorEx.class,"shooter");
 
+        waitForStart();
+        while (opModeIsActive()){
+            preShooter.setVelocity(preShooterVelocity);
+            shooter.setVelocity(shooterVelocity);
+        }
     }
-
-
 }
